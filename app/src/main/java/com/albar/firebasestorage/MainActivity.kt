66cd04e1258b5bfe353215c099dbea74d8a6759e
albarfikri <fikrialbar4@gmail.com>
2021-10-16 15:04:53 +0700

    configuring firebase

--- app/src/main/java/com/albar/firebasestorage/MainActivity.kt
+++ app/src/main/java/com/albar/firebasestorage/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.albar.firebasestorage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.albar.firebasestorage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
