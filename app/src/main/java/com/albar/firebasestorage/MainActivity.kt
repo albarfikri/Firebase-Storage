1e9aaff473745c6c36f79ecf89a0d9780b7927d1
albarfikri <fikrialbar4@gmail.com>
2021-10-17 21:39:51 +0700

    uploading image to firebase successfully

--- app/src/main/java/com/albar/firebasestorage/MainActivity.kt
+++ app/src/main/java/com/albar/firebasestorage/MainActivity.kt
@@ -2,16 +2,23 @@ package com.albar.firebasestorage
 
 import android.net.Uri
 import android.os.Bundle
-import androidx.activity.result.ActivityResultCallback
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.albar.firebasestorage.databinding.ActivityMainBinding
-
-private const val REQUEST_CODE_IMAGE_PICK = 0
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+//    private val imageRef = Firebase.storage.reference
 
     var curFile: Uri? = null
 
@@ -20,15 +27,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        FirebaseApp.initializeApp(this)
+
         val getImage = registerForActivityResult(
-            ActivityResultContracts.GetContent(),
-            ActivityResultCallback {
-                binding.ivImage.setImageURI(it)
-            }
-        )
+            ActivityResultContracts.GetContent()
+        ) {
+            curFile = it
+            binding.ivImage.setImageURI(it)
+        }
 
         binding.ivImage.setOnClickListener() {
             getImage.launch("image/*")
+        }
+
+        binding.btnUpload.setOnClickListener {
+            uploadImageToStorage("myImage")
+        }
+    }
+
+    private fun uploadImageToStorage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            curFile?.let {
+                Firebase.storage.reference.child("images/$filename").putFile(it).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Successfully uploaded Image",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
