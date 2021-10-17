e2d77e7a0c35af874642fa9ae1ba5eeb15345d91
albarfikri <fikrialbar4@gmail.com>
2021-10-17 07:14:56 +0700

    Start Activity with result to take local image (Deprecated)

--- app/src/main/java/com/albar/firebasestorage/MainActivity.kt
+++ app/src/main/java/com/albar/firebasestorage/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.albar.firebasestorage
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.albar.firebasestorage.databinding.ActivityMainBinding
 
+private const val REQUEST_CODE_IMAGE_PICK = 0
+
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    var curFile: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ivImage.setOnClickListener() {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, REQUEST_CODE_IMAGE_PICK)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_IMAGE_PICK) {
+            data?.data?.let {
+                curFile = it
+                binding.ivImage.setImageURI(it)
+            }
+        }
     }
 }
