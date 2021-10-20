f4638c52365ef0d4b70c05803381640e81dceb3d
albarfikri <fikrialbar4@gmail.com>
2021-10-20 12:32:47 +0700

    delete file from firebase

--- app/src/main/java/com/albar/firebasestorage/MainActivity.kt
+++ app/src/main/java/com/albar/firebasestorage/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.albar.firebasestorage
 
+import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -39,15 +42,61 @@ class MainActivity : AppCompatActivity() {
 
         binding.ivImage.setOnClickListener() {
             getImage.launch("image/*")
+            editTextVisibility(true)
         }
 
         binding.btnUpload.setOnClickListener {
-            uploadImageToStorage("myImage")
+            val text = binding.edtImageName.text
+            if (text.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Field cannot be empty.", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                closeKeyboard(binding.btnUpload)
+                uploadImageToStorage(text.toString())
+                editTextVisibility(false)
+            }
         }
 
         binding.btnDownload.setOnClickListener {
             binding.ivImage.clearFocus()
             downloadImage("myImage")
+        }
+
+        binding.btnDelete.setOnClickListener {
+            deleteImage("myImage")
+        }
+    }
+
+    private fun uploadImageToStorage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            curFile?.let {
+                imageRef.child("images/$filename").putFile(it).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Successfully uploaded Image",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun deleteImage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            imageRef.child("images/$filename").delete().await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Image Deleted Successfully", Toast.LENGTH_LONG)
+                    .show()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) { // Moving to main thread
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -69,22 +118,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun uploadImageToStorage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            curFile?.let {
-                imageRef.child("images/$filename").putFile(it).await()
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Successfully uploaded Image",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-        } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-            }
+
+    private fun editTextVisibility(state: Boolean) {
+        if (state) {
+            binding.edtImageName.visibility = View.VISIBLE
+        } else {
+            binding.edtImageName.visibility = View.GONE
         }
+    }
+
+    private fun closeKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
